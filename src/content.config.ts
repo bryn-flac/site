d4b6b1afa8b4ef7289d521de971dac04e28b26f4
Bryn <mortysmith486dx@gmail.com>
2026-08-19 18:09:41 +0200

    feat: apply custom design language and dynamic blog layout

---
+++ src/content.config.ts
@@ -0,0 +1,8 @@
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+  }),
+});
---
+++ src/content/blog/test.md
@@ -0,0 +1,7 @@
+---
+title: "Test of Blog"
+description: "IDK."
+pubDate: 2026-08-19
+---
+
+this is a test 
